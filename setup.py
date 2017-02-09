45b896b3eab264ac7eda694b7835c3b17079c223
happyleaves <happyleaves.tfr@gmail.com>
2017-02-09 18:31:16 -0500

    initial

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='upsmychoice',
+    version='1.0.0',
+    description='Python 3 API for UPS My Choice, a way to track packages.',
+    url='https://github.com/happyleavesaoc/python-upsmychoice/',
+    license='MIT',
+    author='happyleaves',
+    author_email='happyleaves.tfr@gmail.com',
+    packages=['upsmychoice'],
+    install_requires=['beautifulsoup4==4.5.1', 'python-dateutil==2.6.0', 'requests==2.12.4'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ]
+)
